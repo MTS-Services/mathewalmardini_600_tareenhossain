786e878e1a6beb83c490c99d6c77d167838ea5a8
Shariarhosain <shariarhosain131529@gmail.com>
2026-03-30 12:32:39 +0600

    feat: add use-scroll and use-transform hooks for enhanced scroll and transform functionalities

--- src/layout/components/Footer.jsx
+++ src/layout/components/Footer.jsx
@@ -532,7 +532,7 @@ const Footer = () => {
           {/* Logo and Company Description */}
           <div>
             <div style={{ marginBottom: "1.25rem" }}>
-              <img src="/logo.png" alt="B-spoke" style={{ height: "6rem" }} />
+              <img src="https://b-spoke-media.s3.eu-north-1.amazonaws.com/logo.png" alt="B-spoke" style={{ height: "6rem" }} />
             </div>
             <p
               className="text-gray-200 text-sm leading-relaxed"
